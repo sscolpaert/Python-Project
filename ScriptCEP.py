1bc154b0c0ea1315ac291a9793359157fba6ed6d
Samuel <colpaertsalvatore@gmail.com>
2022-12-09 19:46:28 -0300

    Fixing grammatical error

--- ScriptCEP.py
+++ ScriptCEP.py
@@ -1,6 +1,6 @@
 # O script executará uma confirmação do que é um CEP, e o que não é.
 import re
-CEP = str(input('Entre com o número do CEP, com pontos e hífen: \n'))
+CEP = str(input('Entre com o número do CEP, sem ponto mas com hífen: \n'))
 if re.search('\d{3}\d{2}-\d{3}', CEP):
     print('Número CEP validado')
 else:
--- ScriptCPF.py
+++ ScriptCPF.py
@@ -1,6 +1,6 @@
 # O script executará uma confirmação do que é um CPF, e o que não é.
 import re
-CPF = str(input('Entre com o número do CPF, com pontos e hífen: \n'))
+CPF = str(input('Entre com o número do CPF, sem ponto e hífen: \n'))
 if re.search('\d{11}', CPF):
     print('Número CPF validado')
 else:
