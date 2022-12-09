27eab8d2426c935a37e9bcc1fd0078a2c4db99c6
Samuel <colpaertsalvatore@gmail.com>
2022-12-09 19:37:41 -0300

    fixing for 11 caracters

--- ScriptCPF.py
+++ ScriptCPF.py
@@ -1,7 +1,7 @@
 # valida o número do CPF com pontos e hífens
 import re
 CPF = str(input('Entre com o número do CPF, com pontos e hífen: \n'))
-if re.search('\d{3}\d{3}\d{3}\d{2}', CPF):
+if re.search('\d{11}', CPF):
     print('Número CPF validado')
 else:
     print('Número do CPF fora do padrão')
