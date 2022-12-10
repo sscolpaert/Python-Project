671c579692a7d13fd721abf412cae457a36d7f32
Samuel <colpaertsalvatore@gmail.com>
2022-12-10 16:09:43 -0300

    fixing

--- Criação de funçôes em python.py
+++ Criação de funçôes em python.py
@@ -1,6 +1,7 @@
-import math
-def raizQuadrada(numero):
-    return math.sqrt(numero)
+def raizQuadrada(num1, num2):
+    num1 = math.sqrt(num1)
+    num2 = math.sqrt(num2)
+    print (num1," , ", num2)
 
 raizQuadrada()
-9.0
+(9.0, 3.0)
