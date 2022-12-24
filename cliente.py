ffaa61ebd88eefe935a01914849085fba1318b01
Samuel <colpaertsalvatore@gmail.com>
2022-12-24 02:21:29 -0300

    Cliente e Servidor

--- cliente.py
+++ cliente.py
@@ -1,19 +1,19 @@
 # criando um cliente para conexão com o servidor Python
 import socket
-
-host = '127.0.0.1' # mesmo local do servidor
-porta = 8800 # mesmo porta do servidor
-
+ 
+host = '127.0.0.1'  # mesmo local do servidor
+porta = 8800        # mesma porta do servidor
+ 
 soquete = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-envio = (host, porta)
+envio = (host,porta)
 soquete.connect(envio)
-
-print('Digite: S e pressione ENTER para encerrar...')
-print('Digite a Mensagem: ')
+ 
+print('Digite: S e pressione ENTER para encerrar...') 
+print('DIGITE A MENSAGEM: ')
 mensagem = input()
-
-while mensagem not in ('s', 'S')
+ 
+while mensagem not in ('s','S'):
     soquete.send(str(mensagem).encode())
-    mensagem = input
-
+    mensagem = input()
+ 
 soquete.close()
--- servidor.py
+++ servidor.py
@@ -1,26 +1,27 @@
+# CRIANDO UM SERVIDOR PYTHON
 import socket
-
-host = '127.0.0.1' # O mesmo que localhost
-porta = 8800 # Porta da conexão
-
-soquete = socket.socket(socket.AF_INET, socket.SOCK_STREAM) # Estou usando TCP/IP
+from datetime import *
+ 
+host = '127.0.0.1'  # o mesmo que localhost
+porta = 8800         # porta da conexão
+ 
+soquete = socket.socket(socket.AF_INET, socket.SOCK_STREAM) #estou usando TCP/IP
 recebe = (host, porta)
 soquete.bind(recebe)
 soquete.listen(2)
-
-print('\nServidor Iniciado...IP: ', host, 'PORTA: ', porta)
-
+ 
+print('\nSERVIDOR INICIADO...IP: ', host, 'PORTA: ', porta,' EM: ',datetime.now().strftime('%d/%m/%Y - %H:%M:%S'))
+ 
 while True:
     conexao, enderecoIP = soquete.accept()
-    print('\Servidor Acessado pelo Cliente:', enderecoIP, 'EM: ', datetime.now().strftime('%d/%m/%y - %H:%M:%S'))
-
+    print('\nSERVIDOR ACESSADO PELO CLIENTE:', enderecoIP, 'EM: ',datetime.now().strftime('%d/%m/%Y - %H:%M:%S'))
+ 
     while True:
         mensagem = conexao.recv(2048)
         if not mensagem:
             break
-        print('\nIP Cliente:', enderecoIP)
-        print('Mensagem Recebida: ', mensagem.decode(),' - ', datetime.now().strftime('%H:%M:%S'))
-
-    print('Conexão com o Cliente Finalizada...', enderecoIP, ' EM: ', datetime.now().strftime('%d/%m/%y - %H:%M:%S'))
+        print('\nIP CLIENTE:', enderecoIP)
+        print('MENSAGEN RECEBIDA: ', mensagem.decode(),' - ',datetime.now().strftime('%H:%M:%S'))
+ 
+    print('CONEXÃO COM O CLIENTE FINALIZADA...', enderecoIP, ' EM: ',datetime.now().strftime('%d/%m/%Y - %H:%M:%S'))
     conexao.close()
-        
